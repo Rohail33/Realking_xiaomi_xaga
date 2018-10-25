0608e78a9e3224a577a749f457c8c63ebeb524f5
kdrag0n <dragon@khronodragon.com>
2018-10-24 19:09:01 -0700

    selinux: don't require auditing
    Audit is bad. It affects performance under all circumstances. It's also
    dirty.
    
    From LWN: "Andy submitted a patch to fix this particular problem, but he
    didn't stop there. He has come to the conclusion that the audit
    subsystem is beyond repair, so his patch marks the whole thing as being
    broken, making it generally inaccessible. He cited a number of problems
    beyond this security issue: it hurts performance even when it is not
    being used, it is not (in his mind) reliable, it has problems with
    various architectures, and "its approach to freeing memory is
    terrifying." All told, Andy said, we're better off without it"
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -424,6 +424,7 @@ static inline int avc_xperms_audit(struct selinux_state *state,
 				   u8 perm, int result,
 				   struct common_audit_data *ad)
 {
+#ifdef CONFIG_AUDIT
 	u32 audited, denied;
 
 	audited = avc_xperms_audit_required(
@@ -432,6 +433,9 @@ static inline int avc_xperms_audit(struct selinux_state *state,
 		return 0;
 	return slow_avc_audit(state, ssid, tsid, tclass, requested,
 			audited, denied, result, ad);
+#else
+	return 0;
+#endif
 }
 
 static void avc_node_free(struct rcu_head *rhead)
@@ -663,6 +667,7 @@ found:
 	return node;
 }
 
+#ifdef CONFIG_AUDIT
 /**
  * avc_audit_pre_callback - SELinux specific information
  * will be called by generic audit code
@@ -791,6 +796,7 @@ static void avc_audit_post_callback(struct audit_buffer *ab, void *a)
 	}
 }
 
+
 /* This is the slow part of avc audit with big stack footprint */
 noinline int slow_avc_audit(struct selinux_state *state,
 			    u32 ssid, u32 tsid, u16 tclass,
@@ -826,6 +832,7 @@ noinline int slow_avc_audit(struct selinux_state *state,
 	common_lsm_audit(a, avc_audit_pre_callback, avc_audit_post_callback);
 	return 0;
 }
+#endif
 
 /**
  * avc_add_callback - Register a callback for security events.
--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3790,6 +3790,7 @@ out:
 	return match;
 }
 
+#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3812,6 +3813,7 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
+#endif
 
 #ifdef CONFIG_NETLABEL
 /**
