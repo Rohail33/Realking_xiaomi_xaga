6688c0d0b1dcf327ed8c36c3dd897229f0ce4c58
Pierre Gondois <Pierre.Gondois@arm.com>
2021-05-04 10:07:43 +0100

    sched/fair: Fix negative energy delta in find_energy_efficient_cpu()
    find_energy_efficient_cpu() (feec()) searches the best energy CPU
    to place a task on. To do so, compute_energy() estimates the energy
    impact of placing the task on a CPU, based on CPU and task utilization
    signals.
    
    Utilization signals can be concurrently updated while evaluating a
    performance domain (pd). In some cases, this leads to having a
    'negative delta', i.e. placing the task in the pd is seen as an
    energy gain. Thus, any further energy comparison is biased.
    
    In case of a 'negative delta', return prev_cpu since:
    1. a 'negative delta' happens in less than 0.5% of feec() calls,
       on a Juno with 6 CPUs (4 little, 2 big)
    2. it is unlikely to have two consecutive 'negative delta' for
       a task, so if the first call fails, feec() will correctly
       place the task in the next feec() call
    3. EAS current behavior tends to select prev_cpu if the task
       doesn't raise the OPP of its current pd. prev_cpu is EAS's
       generic decision
    4. prev_cpu should be preferred to returning an error code.
       In the latter case, select_idle_sibling() would do the placement,
       selecting a big (and not energy efficient) CPU. As 3., the task
       would potentially reside on the big CPU for a long time
    
    Reported-by: Xuewen Yan <xuewen.yan@unisoc.com>
    Suggested-by: Xuewen Yan <xuewen.yan@unisoc.com>
    Signed-off-by: Pierre Gondois <Pierre.Gondois@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Lukasz Luba <lukasz.luba@arm.com>
    Reviewed-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Reviewed-by: Vincent Donnefort <vincent.donnefort@arm.com>
    Link: https://lkml.kernel.org/r/20210504090743.9688-3-Pierre.Gondois@arm.com
    
    # Conflicts:
    #	kernel/sched/fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6856,7 +6856,7 @@ static int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu, int sy
 	unsigned long cpu_cap, util, base_energy = 0;
 	bool boosted, latency_sensitive = false;
 	unsigned int min_exit_lat = UINT_MAX;
-	int cpu, best_energy_cpu = prev_cpu;
+	int cpu, best_energy_cpu = prev_cpu, target = -1;
 	struct cpuidle_state *idle;
 	struct sched_domain *sd;
 	struct perf_domain *pd;
@@ -6870,7 +6870,7 @@ static int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu, int sy
 	rcu_read_lock();
 	pd = rcu_dereference(rd->pd);
 	if (!pd || READ_ONCE(rd->overutilized))
-		goto fail;
+		goto unlock;
 
 	cpu = smp_processor_id();
 	if (sync && cpu_rq(cpu)->nr_running == 1 &&
@@ -6888,7 +6888,9 @@ static int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu, int sy
 	while (sd && !cpumask_test_cpu(prev_cpu, sched_domain_span(sd)))
 		sd = sd->parent;
 	if (!sd)
-		goto fail;
+		goto unlock;
+
+	target = prev_cpu;
 
 	if (!task_util_est(p) && p_util_min == 0)
 		goto unlock;
@@ -6949,6 +6951,8 @@ static int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu, int sy
 			/* Always use prev_cpu as a candidate. */
 			if (!latency_sensitive && cpu == prev_cpu) {
 				prev_delta = compute_energy(p, prev_cpu, pd);
+				if (prev_delta < base_energy_pd)
+					goto unlock;
 				prev_delta -= base_energy_pd;
 				best_delta = min(best_delta, prev_delta);
 			}
@@ -6990,6 +6994,8 @@ static int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu, int sy
 		if (!latency_sensitive && max_spare_cap_cpu >= 0 &&
 						max_spare_cap_cpu != prev_cpu) {
 			cur_delta = compute_energy(p, max_spare_cap_cpu, pd);
+			if (cur_delta < base_energy_pd)
+				goto unlock;
 			cur_delta -= base_energy_pd;
 			if (cur_delta < best_delta) {
 				best_delta = cur_delta;
@@ -6997,7 +7003,6 @@ static int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu, int sy
 			}
 		}
 	}
-unlock:
 	rcu_read_unlock();
 
 	if (latency_sensitive)
@@ -7007,18 +7012,16 @@ unlock:
 	 * Pick the best CPU if prev_cpu cannot be used, or if it saves at
 	 * least 6% of the energy used by prev_cpu.
 	 */
-	if (prev_delta == ULONG_MAX)
-		return best_energy_cpu;
+	if ((prev_delta == ULONG_MAX) ||
+	    (prev_delta - best_delta) > ((prev_delta + base_energy) >> 4))
+		target = best_energy_cpu;
 
-	if ((prev_delta - best_delta) > ((prev_delta + base_energy) >> 4))
-		return best_energy_cpu;
+	return target;
 
-	return prev_cpu;
-
-fail:
+unlock:
 	rcu_read_unlock();
 
-	return -1;
+	return target;
 }
 
 /*
