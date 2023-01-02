6c6f92f11f9b4d7430c84d3bf4fd295876b28caf
Rohail33 <rohailyousafzai49@gmail.com>
2023-01-02 17:08:36 +0000

    Buildtools: Adding banner

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=DynamIQ kernel by nakixii @ xda-developers
+kernel.string= Kernel BY @Rohail33 -- TG 
 do.devicecheck=0
 do.modules=0
 do.systemless=0
