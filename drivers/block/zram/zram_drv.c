32a0872448483eed8d96e8099293813d03321be5
zzh-zzh-zzh <1292882190@qq.com>
2023-01-18 10:38:32 +0800

    zram: Remove comp_algorithm_store function
    We don't need this function after we set comp_algorithm read only,so remove it.
    
    Change-Id: Iebdb84cb7ed169f1fce4ad54b825c48751115afa

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1003,7 +1003,7 @@ static ssize_t comp_algorithm_show(struct device *dev,
 	return sz;
 }
 
-static ssize_t comp_algorithm_store(struct device *dev,
+/*static ssize_t comp_algorithm_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t len)
 {
 	struct zram *zram = dev_to_zram(dev);
@@ -1011,7 +1011,6 @@ static ssize_t comp_algorithm_store(struct device *dev,
 	size_t sz;
 
 	strlcpy(compressor, buf, sizeof(compressor));
-	/* ignore trailing newline */
 	sz = strlen(compressor);
 	if (sz > 0 && compressor[sz - 1] == '\n')
 		compressor[sz - 1] = 0x00;
@@ -1030,6 +1029,7 @@ static ssize_t comp_algorithm_store(struct device *dev,
 	up_write(&zram->init_lock);
 	return len;
 }
+*/
 
 static ssize_t compact_store(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t len)
