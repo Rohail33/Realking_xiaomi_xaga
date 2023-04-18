24c00b5ce9158ee66722c558829ccad2a21e3e1a
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2023-04-18 17:38:59 +0000

    fpsgo_v3 / fpsgo_cus : Fix Path to Header

--- drivers/misc/mediatek/performance/fpsgo_v3/fpsgo_cus/src/fpsgo_main.c
+++ drivers/misc/mediatek/performance/fpsgo_v3/fpsgo_cus/src/fpsgo_main.c
@@ -40,7 +40,7 @@
 #endif
 #include "fpsgo_ko.h"
 
-#line __LINE__ "vendor/mediatek/kernel_modules/fpsgo_cus/src/fpsgo_main.c"
+#line __LINE__ "drivers/misc/mediatek/performance/fpsgo_v3/fpsgo_cus/src/fpsgo_main.c"
 
 static void __exit fpsgo_exit(void) {}
 
