267394110efabd659eda20183a41f386fbb552c0
ReaperSigma <vidium400@gmail.com>
2023-12-16 16:11:59 +0000

    KERNEL:based on this commit:https://github.com/Pzqqt/android_kernel_xiaomi_marble/commit/3b15dea910ad3c32caa697ff4bf9ea4c0eb94d74 this create a custom module blacklisted, so this blacklist zram and zsmalloc for not loading those modules in staging process and enable via defconfig

--- kernel/module.c
+++ kernel/module.c
@@ -3554,13 +3554,17 @@ int __weak module_frob_arch_sections(Elf_Ehdr *hdr,
 
 /* module_blacklist is a comma-separated list of module names */
 static char *module_blacklist;
+static char *custom_module_blacklist[] = {
+	"zram","zsmalloc"
+};
 static bool blacklisted(const char *module_name)
 {
 	const char *p;
 	size_t len;
+	int i;
 
 	if (!module_blacklist)
-		return false;
+		goto custom_blacklist;
 
 	for (p = module_blacklist; *p; p += len) {
 		len = strcspn(p, ",");
@@ -3569,6 +3573,11 @@ static bool blacklisted(const char *module_name)
 		if (p[len] == ',')
 			len++;
 	}
+	custom_blacklist:
+	for (i = 0; i < ARRAY_SIZE(custom_module_blacklist); i++)
+		if (!strcmp(module_name, custom_module_blacklist[i]))
+			return true;
+
 	return false;
 }
 core_param(module_blacklist, module_blacklist, charp, 0400);
